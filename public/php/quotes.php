bc8efe4541190874b61316ee6808b9747082baaa
fdiebel <fdiebel@emrl.com>
2015-09-03 16:44:18 -0700

    *yawn*

--- public/php/quotes.php
+++
@@ -1,4 +0,0 @@
-<?php $quotes = file("/srv/httpd/htdocs/hosted/projects.emrl.com/cit/public/php/quotes.txt");
-                                  srand((double)microtime() * 1000000);
-                                  $ranNum = rand(0, count($quotes)-1); 
-                                  echo $quotes[$ranNum]; ?>
